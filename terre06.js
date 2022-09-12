837ad7595fc23fc8b8a47f5746c0f51ae9a31a22
0xUndersun <alex.atc.dev@gmail.com>
2022-09-12 17:16:31 +0200

    [6.100%] exo 6 division finished

---
+++ terre06.js
@@ -0,0 +1,13 @@
+// calcul division
+let Number1 = parseInt(process.argv[2])
+let Number2 = parseInt(process.argv[3])
+
+let reste = Number1 % Number2
+let resultat = (Number1 - reste) / Number2
+
+if (Number2 === 0 || Number2 > Number1) {
+    console.log("erreur")
+} else {
+    console.log("resultat: " + resultat + "\n" + "reste: " + reste)
+}
+
