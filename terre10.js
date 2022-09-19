77032cb4e993e3d66d189bc91d0d21b3b57ab783
0xUndersun <alex.atc.dev@gmail.com>
2022-09-19 12:00:43 +0200

    [FEATURE]exercise 10 racine carree with error gestion and with no method .sqrt

---
+++ terre10.js
@@ -0,0 +1,27 @@
+// fonction racine carree
+function racineCarree(nombre) {
+    let racine = 0;
+    let puissance = 0;
+    while (puissance <= nombre) {
+        racine++;
+        puissance = racine * racine;
+    }
+    return racine - 1;
+}
+// gestion d'erreurs
+if (process.argv[2] === undefined) {
+    console.log("erreur");
+    process.exit(1);
+}
+if (process.argv.length > 3 || parseInt(process.argv[2]) === NaN) {
+    console.log("erreur");
+    process.exit(1);
+}
+if (process.argv[2] < 0) {
+    console.log("erreur");
+    process.exit(1);
+}
+
+// affichage du resultat
+let resultat = parseInt(process.argv[2])
+console.log(racineCarree(resultat))
