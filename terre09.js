000014b8a054afc479d630e06d95df47457fb4ec
0xUndersun <alex.atc.dev@gmail.com>
2022-09-19 11:51:31 +0200

    [FEATURE] Exercise 09 calcul power with error handler

---
+++ terre09.js
@@ -0,0 +1,30 @@
+function calculPuissance(args2, args3) {
+  let puissance = 1;
+  for (let index = 0; index < args3; index++) {
+    puissance = puissance * args2;
+  }
+  return puissance;
+}
+
+if (process.argv.length < 4) {
+  console.log("erreur : pas assez d'arguments");
+  process.exit(1);
+} else if (process.argv.length > 4) {
+    console.log("erreur : trop d'arguments");
+    process.exit(1);
+} else if (parseInt(process.argv[2]) === NaN || parseInt(process.argv[3]) === NaN) {
+    console.log("erreur : arguments non numériques");
+    process.exit(1);
+} // si args2 ou args3 n'est pas un nombre, alors parseInt renvoie NaN
+else if (process.argv[2] < 0 || process.argv[3] < 0) {
+    console.log("erreur : arguments négatifs");
+    process.exit(1);
+} else if (process.argv[2] === 0 && process.argv[3] === 0) {
+    console.log("erreur : arguments nuls");
+    process.exit(1);
+}
+
+
+
+let resultat = calculPuissance(process.argv[2], process.argv[3])
+console.log(resultat)
